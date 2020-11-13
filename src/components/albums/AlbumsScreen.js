514789d589382732aed545c3b9405043f0fbe63c
David Brito <dbrito@exceptia.es>
2020-11-13 15:49:18 +0100

    React navigation

---
+++ src/components/albums/AlbumsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const AlbumsScreen = () => {
+  return (
+    <View>
+      <Text>Discos</Text>
+    </View>
+  );
+};
+
+export default AlbumsScreen;
